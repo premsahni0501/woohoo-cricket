41ede2640d2f01d414691491b53ecd73cdc70d3b
premsahni0501 <premsahni0501@gmail.com>
2020-12-13 18:24:10 +0530

    check ip

--- pages/api/news/index.js
+++ pages/api/news/index.js
@@ -1,12 +1,16 @@
 import cheerio from 'cheerio'
 import axios from 'axios'
+var os = require('os');
 
+var networkInterfaces = os.networkInterfaces();
+
+console.log(networkInterfaces);
 export const getNews = (page, loadmore) => {
   return new Promise(async resolve => {
     const url = `https://cricket.yahoo.net/news/page/${page}?loadmore=${loadmore}&id=dab9f7bf-65a9-49cb-90b8-61c0b65ee0d8&extentities=`
     const res = await axios.get(url)
     const $ = cheerio.load(res.data)
-    console.log(res.data)
+    console.log(Intl.DateTimeFormat().resolvedOptions().locale)
     const newsItems = []
     $('.article-item').each((index, el) => {
       const news_link = $(el).find('.article-thumbnail > a').attr('href').trim()
@@ -20,6 +24,11 @@ export const getNews = (page, loadmore) => {
   })
 }
 export default async (req, res) => {
-  const data = await getNews(1, true)
-  res.json(data)
+  try {
+    const data = await getNews(1, true)
+    res.json(data)
+  }
+  catch (e) {
+    console.log(e)
+  }
 }
